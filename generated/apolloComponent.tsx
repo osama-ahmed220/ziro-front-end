e4b603edef010dd91a43e7d8c3639085266de55b
Osama Ahmed <osama.ahmed220@gmail.com>
2019-09-27 21:53:25 +0500

    some changes

--- generated/apolloComponent.tsx
+++ generated/apolloComponent.tsx
@@ -1,4 +1,79 @@
-import Maybe from "graphql/tsutils/Maybe";
+export type Maybe<T> = T | null;
+
+export interface CreateUserInput {
+  firstName: string;
+
+  lastName: string;
+
+  email: string;
+
+  password: string;
+}
+
+export interface UpdateUserInput {
+  firstName: string;
+
+  lastName: string;
+
+  email: string;
+
+  password: string;
+}
+
+export interface CreateTodoInput {
+  title: string;
+
+  isDone?: Maybe<string>;
+}
+
+export interface UpdateTodoInput {
+  title: string;
+
+  isDone?: Maybe<string>;
+}
+
+// ====================================================
+// Documents
+// ====================================================
+
+export type LoginVariables = {
+  email: string;
+  password: string;
+};
+
+export type LoginMutation = {
+  __typename?: "Mutation";
+
+  login: LoginLogin;
+};
+
+export type LoginLogin = {
+  __typename?: "User";
+
+  id: string;
+
+  name: string;
+
+  email: string;
+};
+
+export type LogoutVariables = {};
+
+export type LogoutMutation = {
+  __typename?: "Mutation";
+
+  logout: boolean;
+};
+
+export type MeVariables = {};
+
+export type MeQuery = {
+  __typename?: "Query";
+
+  me: Maybe<MeMe>;
+};
+
+export type MeMe = UserBasicFragmentFragment;
 
 export type UserBasicFragmentFragment = {
   __typename?: "User";
@@ -16,10 +91,157 @@ export type UserBasicFragmentFragment = {
   password: string;
 };
 
-export type MeQuery = {
-  __typename?: "Query";
+import * as ReactApollo from "react-apollo";
+import * as React from "react";
 
-  me: Maybe<MeMe>;
-};
+import gql from "graphql-tag";
 
-export type MeMe = UserBasicFragmentFragment;
+// ====================================================
+// Fragments
+// ====================================================
+
+export const UserBasicFragmentFragmentDoc = gql`
+  fragment UserBasicFragment on User {
+    id
+    firstName
+    lastName
+    name
+    email
+    password
+  }
+`;
+
+// ====================================================
+// Components
+// ====================================================
+
+export const LoginDocument = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      id
+      name
+      email
+    }
+  }
+`;
+export class LoginComponent extends React.Component<
+  Partial<ReactApollo.MutationProps<LoginMutation, LoginVariables>>
+> {
+  render() {
+    return (
+      <ReactApollo.Mutation<LoginMutation, LoginVariables>
+        mutation={LoginDocument}
+        {...(this as any)["props"] as any}
+      />
+    );
+  }
+}
+export type LoginProps<TChildProps = any> = Partial<
+  ReactApollo.MutateProps<LoginMutation, LoginVariables>
+> &
+  TChildProps;
+export type LoginMutationFn = ReactApollo.MutationFn<
+  LoginMutation,
+  LoginVariables
+>;
+export function LoginHOC<TProps, TChildProps = any>(
+  operationOptions:
+    | ReactApollo.OperationOption<
+        TProps,
+        LoginMutation,
+        LoginVariables,
+        LoginProps<TChildProps>
+      >
+    | undefined
+) {
+  return ReactApollo.graphql<
+    TProps,
+    LoginMutation,
+    LoginVariables,
+    LoginProps<TChildProps>
+  >(LoginDocument, operationOptions);
+}
+export const LogoutDocument = gql`
+  mutation Logout {
+    logout
+  }
+`;
+export class LogoutComponent extends React.Component<
+  Partial<ReactApollo.MutationProps<LogoutMutation, LogoutVariables>>
+> {
+  render() {
+    return (
+      <ReactApollo.Mutation<LogoutMutation, LogoutVariables>
+        mutation={LogoutDocument}
+        {...(this as any)["props"] as any}
+      />
+    );
+  }
+}
+export type LogoutProps<TChildProps = any> = Partial<
+  ReactApollo.MutateProps<LogoutMutation, LogoutVariables>
+> &
+  TChildProps;
+export type LogoutMutationFn = ReactApollo.MutationFn<
+  LogoutMutation,
+  LogoutVariables
+>;
+export function LogoutHOC<TProps, TChildProps = any>(
+  operationOptions:
+    | ReactApollo.OperationOption<
+        TProps,
+        LogoutMutation,
+        LogoutVariables,
+        LogoutProps<TChildProps>
+      >
+    | undefined
+) {
+  return ReactApollo.graphql<
+    TProps,
+    LogoutMutation,
+    LogoutVariables,
+    LogoutProps<TChildProps>
+  >(LogoutDocument, operationOptions);
+}
+export const MeDocument = gql`
+  query Me {
+    me {
+      ...UserBasicFragment
+    }
+  }
+
+  ${UserBasicFragmentFragmentDoc}
+`;
+export class MeComponent extends React.Component<
+  Partial<ReactApollo.QueryProps<MeQuery, MeVariables>>
+> {
+  render() {
+    return (
+      <ReactApollo.Query<MeQuery, MeVariables>
+        query={MeDocument}
+        {...(this as any)["props"] as any}
+      />
+    );
+  }
+}
+export type MeProps<TChildProps = any> = Partial<
+  ReactApollo.DataProps<MeQuery, MeVariables>
+> &
+  TChildProps;
+export function MeHOC<TProps, TChildProps = any>(
+  operationOptions:
+    | ReactApollo.OperationOption<
+        TProps,
+        MeQuery,
+        MeVariables,
+        MeProps<TChildProps>
+      >
+    | undefined
+) {
+  return ReactApollo.graphql<
+    TProps,
+    MeQuery,
+    MeVariables,
+    MeProps<TChildProps>
+  >(MeDocument, operationOptions);
+}
